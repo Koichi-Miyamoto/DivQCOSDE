5ee66795237185410d11ba6423d145d80eebf11a
miyamone <monmon38mon@gmail.com>
2025-01-29 08:51:27 +0900

    - modify RQAE so that the Grover num in the final round is 1/epsilon

--- QAE/RQAE.py
+++ QAE/RQAE.py
@@ -3,21 +3,25 @@ import pandas as pd
 from .MaximizeL import MaximizeLikelihood
 from .QAEResult import QAEResult
 
-def RQAE(a, K, R):
+def RQAE(a, epsilon, R):
 
     theta = np.arcsin(np.sqrt(a))
     thetaMuls = []
     nShots = []
     n1s = []
+    intEpsInv = int(np.ceil(1 / epsilon))
+    K = int(np.log2(1 / epsilon))
+    nRound = K + 1 if 2**K < intEpsInv else K
 
-    for i in range(1, K+1):
-        if i == 1:
+    for i in range(nRound):
+        if i == 0:
             n1 = np.random.binomial(R, a)
             thetaMuls.append(1)
             nShots.append(R)
             n1s.append(n1)
         else:
-            for nOra in np.random.randint(2**(i-1), 2**i, size=R):
+            maxM = intEpsInv if i == K else 2**(i+1)
+            for nOra in np.random.randint(2**i, maxM, size=R):
                 n1 = np.random.binomial(1, np.sin(nOra * theta)**2)
                 thetaMuls.append(nOra)
                 nShots.append(1)
