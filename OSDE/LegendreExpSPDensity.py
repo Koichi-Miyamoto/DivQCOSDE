d33b3f4b2382030cdad0694d84fd135a354025da
miyamone <monmon38mon@gmail.com>
2025-02-10 09:06:17 +0900

    add results

--- OSDE/LegendreExpSPDensity.py
+++ OSDE/LegendreExpSPDensity.py
@@ -17,20 +17,20 @@ def LegExp(f, maxDeg):
     for l in range(1, maxDeg+1):
         legPoly = sp.special.legendre(l)
         integFunc = lambda x: legPoly(x) * f(x)
-        integ, _ = sp.integrate.quad(integFunc, -1.0, 1.0)
+        integ, _ = sp.integrate.quad(integFunc, -1, 1)
         coefs[l] = (l + 0.5) * integ
 
     return LegExpResult(coefs)
 
-def LegExpSPDensity(densFunc0, transDensFunc, maxDeg):
+def LegExpSPDensity(densFunc0, transDensFunc, maxDeg, lbx=-1, ubx=1, epsabs=1.49e-8):
 
     coefs = np.zeros(maxDeg+1)
     coefs[0] = 0.5 # assume that f is a PDF on [-1,1]
 
     for l in range(1, maxDeg+1):
         legPoly = sp.special.legendre(l)
-        integFunc = lambda x0, x: legPoly(x) * densFunc0(x0) * transDensFunc(x, x0)
-        integ, _ = sp.integrate.dblquad(integFunc, -1.0, 1.0, -1.0, 1.0)
+        integFunc = lambda x, x0: legPoly(x) * densFunc0(x0) * transDensFunc(x, x0)
+        integ, _ = sp.integrate.dblquad(integFunc, -1, 1, lbx, ubx, epsabs=epsabs)
         coefs[l] = (l + 0.5) * integ
 
     return LegExpResult(coefs)
